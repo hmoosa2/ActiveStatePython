523d1e9c7839f666e7fa4eda967ebc7ee3368249
hmoosa2 <hmoosa2@uclan.ac.uk>
2023-11-30 15:23:15 +0000

    radio button pack tutorial complete

--- GridButton.py
+++ GridButton.py
@@ -7,13 +7,31 @@ root = Tk ()
 helloButton = Button (root, text="Press me!" , command = cmdfunction)
 helloButton.grid (row = 0 , column = 0, sticky=N, padx=10, pady=10)
 
-helloButton2 = Button (root, text="Press me!" , command = cmdfunction)
+helloButton2 = Button (root, text="Press me 2!" , command = cmdfunction)
 helloButton2.grid (row = 0 , column = 1, sticky=E, padx=10, pady=10)
 
-helloButton3 = Button (root, text="Press me!" , command = cmdfunction)
+helloButton3 = Button (root, text="Press me 3!" , command = cmdfunction)
 helloButton3.grid (row = 0 , column = 2, sticky=S, padx=10, pady=10)
 
-helloButton4 = Button (root, text="Press me!" , command = cmdfunction)
+helloButton4 = Button (root, text="Press me 4!" , command = cmdfunction)
 helloButton4.grid (row = 0 , column = 3, sticky=W, padx=10, pady=10)
+
+root.grid_rowconfigure(0, minsize=100)
+
+root.grid_rowconfigure(1, minsize=100)
+
+root.grid_rowconfigure(2, minsize=100)
+
+root.grid_rowconfigure(3, minsize=100)
+
+
+root.grid_columnconfigure(0, minsize=100)
+
+root.grid_columnconfigure(1, minsize=100)
+
+root.grid_columnconfigure(2, minsize=100)
+
+root.grid_columnconfigure(3, minsize=100)
+
 
 root.mainloop ()
--- PackRadioButton.py
+++ PackRadioButton.py
@@ -1,11 +1,18 @@
 from tkinter import *
 
+def cmdfunction ():
+  print ("Hello")
+  print(var.get())
+
 root = Tk()
 
-radiobutton1 = Radiobutton(root, text="Option 1", value=1)
-radiobutton1.pack()
+var = IntVar()
+var.set(0)
 
-radiobutton2 = Radiobutton(root, text="Option 2", value=2)
-radiobutton2.pack()
+radiobutton1 = Radiobutton(root, text="Option 1", value=1, variable=var, command=cmdfunction)
+radiobutton1.pack(side=LEFT, fill=BOTH, expand=True)
 
-root.mainloop ()
+radiobutton2 = Radiobutton(root, text="Option 2", value=2, variable=var, command=cmdfunction)
+radiobutton2.pack(side=LEFT, fill=BOTH, expand=True)
+
+root.mainloop()
